2efe4d7b31dfcb639758c4f92d9b49e7342d5244
sofie5555 <72884166+sofie5555@users.noreply.github.com>
2021-04-09 11:32:28 +0200

    Test

---
+++ Dogs/Activity1Test.java
@@ -0,0 +1,35 @@
+
+public class Activity1Test {
+
+	public static void main(String[] args) {
+
+		/* Create 3 dog objects:
+			 *
+			 *  The first:
+			 *   - name is "Gerald"
+			 *   - isBoy is true
+			 *   - age is 4
+			 *   - weight is 13.2
+			 *   - breed is "Australian Shepard"
+			 *
+			 *  The second:
+			 *   - name is "Mackguire"
+			 *   - isBoy is false
+			 *   - age is 8
+			 *   - weight is 5.7
+			 *   - breed is "Corgi"
+			 *
+			 *
+			 *  The third:
+			 *   - name is "Bartholomew"
+			 *   - isBoy is false
+			 *   - age is 12
+			 *   - weight is 10.3
+			 *   - breed is "Poodle"
+			 */
+
+		// Create a #toString method that shows all the details of the dog, if possible.
+
+	}
+
+}
